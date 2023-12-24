2c9cc3ed64bde2ffbc988af2db53eee33abccf24
parteek kumar <digitalparteek@gmail.com>
2023-12-24 19:01:03 +0530

    basic get application complete

--- api.go
+++ api.go
@@ -72,7 +72,9 @@ func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request )error 
 }
 
 func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request )error {
-	return nil
+	account := NewAccount("Parteek", "Kumar")
+	
+	return WriteJSON(w, http.StatusOK, account)
 }
 
 func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request )error {
