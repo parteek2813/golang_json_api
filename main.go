8ead19c04fcab58dac0ca74a74068601fe8f140a
parteek kumar <digitalparteek@gmail.com>
2023-12-25 09:57:40 +0530

    postgres dockerization implemented.

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"log"
+)
 
 func main() {
 
-	server := NewAPIServer(":3000")
+	store, err := NewPostgresStore()
+	if err != nil {
+		log.Fatal(err)
+	}
 
+
+
+	server := NewAPIServer(":3000", store)
 	server.Run()
 
-	fmt.Println("Yeah buddy!")
 }
---
+++ storage.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+type Storage interface {
+	CreateAccount(*Account) error
+	UpdateAccount(*Account) error
+	DeleteAccount(int) error
+	GetAccountByID(int) (*Account, error)
+}
+
+type PostgresStore struct {
+	db *sql.DB
+}
+
+func NewPostgresStore() (*PostgresStore, error) {
+	connStr := "user=postgres dbname=postgres password=gobank sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+	
+	// If error comes
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return &PostgresStore{
+		db : db,
+	}, nil
+
+
+}
+
+func (s *PostgresStore) CreateAccount(*Account) error {
+	return nil
+}
+
+func (s *PostgresStore) UpdateAccount(*Account) error {
+	return nil
+}
+func (s *PostgresStore) DeleteAccount(id int) error {
+	return nil
+}
+func (s *PostgresStore) GetAccountByID(id int) (*Account, error) {
+	return nil, nil
+}
