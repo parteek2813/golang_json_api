a414c465248aff397c7cf0bd77618b80b2c655d5
parteek kumar <digitalparteek@gmail.com>
2023-12-26 13:58:12 +0530

    get account by id api complete

--- api.go
+++ api.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -72,13 +73,31 @@ func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request )err
 }
 
 
-func (s *APIServer) handleGetAccountByID(w http.ResponseWriter, r *http.Request )error {
-	id := mux.Vars(r)["id"]
 
-	fmt.Println(id)
+
+func (s *APIServer) handleGetAccountByID(w http.ResponseWriter, r *http.Request )error {
+	
+	// The id can be of string to .... so for checking convert it into the int 
+
+	idStr := mux.Vars(r)["id"]
+
+	id, err := strconv.Atoi(idStr)
+
+	if err != nil {
+		return fmt.Errorf("invalid id given %s", idStr)
+	}
+
+	account, err := s.store.GetAccountByID(id)
+
+	if err != nil {
+		return err
+	}
 		
-	return WriteJSON(w, http.StatusOK, &Account{})
+	return WriteJSON(w, http.StatusOK, account)
 }
+
+
+
 
 func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request )error {
 
@@ -115,7 +134,7 @@ func WriteJSON(w http.ResponseWriter, status int , v any) error{
 type apiFunc func(http.ResponseWriter, *http.Request) error 
 
 type ApiError struct {
-	Error string
+	Error string `json:"error"`
 }
 
 func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
--- storage.go
+++ storage.go
@@ -90,8 +90,24 @@ func (s *PostgresStore) DeleteAccount(id int) error {
 }
 
 
+
+// GET ACCOUNT BY ID
 func (s *PostgresStore) GetAccountByID(id int) (*Account, error) {
-	return nil, nil
+	
+	
+	rows, err := s.db.Query("select * from account where id = $1", id)
+
+	if err != nil {
+		return nil, err
+	}
+	
+	for rows.Next(){
+		return scanIntoAccount(rows)
+	}
+
+
+	return nil, fmt.Errorf("account %d not found", id)
+
 }
 
 
@@ -105,15 +121,7 @@ func (s *PostgresStore) GetAccounts() ([]*Account, error) {
 
 	accounts := []*Account{}
 	for rows.Next() {
-		account := new(Account)
-		err := rows.Scan(
-			&account.ID, 
-			&account.FirstName, 
-			&account.LastName,
-			&account.Number,
-			&account.Balance,
-			&account.CreatedAt,
-			); 
+		account, err := scanIntoAccount(rows)
 
 		if err != nil {
 			return nil, err
@@ -124,3 +132,25 @@ func (s *PostgresStore) GetAccounts() ([]*Account, error) {
 
 	return accounts, nil
 }
+
+func scanIntoAccount(rows *sql.Rows) (*Account, error){
+	account := new(Account)
+	err := rows.Scan(
+		&account.ID, 
+		&account.FirstName, 
+		&account.LastName,
+		&account.Number,
+		&account.Balance,
+		&account.CreatedAt,
+		); 
+
+	return account, err
+}
+
+
+
+
+
+
+
+
